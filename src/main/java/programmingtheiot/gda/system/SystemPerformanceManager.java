823734e1f908886f0f30980aa8b0048c44f62582
Nicolas Gallo <nicolas.gallo800@gmail.com>
2025-02-17 18:28:02 +0100

    PIOT-GDA-02-000, PIOT-GDA-02-001 and PIOT-GDA-02-002 terminated and implemented.

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -8,6 +8,8 @@
 
 package programmingtheiot.gda.system;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -18,6 +20,7 @@ import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
 import programmingtheiot.data.SystemPerformanceData;
+import programmingtheiot.gda.app.GatewayDeviceApp;
 
 /**
  * Shell representation of class for student implementation.
@@ -26,8 +29,11 @@ import programmingtheiot.data.SystemPerformanceData;
 public class SystemPerformanceManager
 {
 	// private var's
-	
-	
+	private static final Logger _Logger =
+			Logger.getLogger(SystemPerformanceManager.class.getName());
+
+	private int pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
+
 	// constructors
 	
 	/**
@@ -36,6 +42,11 @@ public class SystemPerformanceManager
 	 */
 	public SystemPerformanceManager()
 	{
+		this.pollRate = ConfigUtil.getInstance().getInteger(ConfigConst.GATEWAY_DEVICE,ConfigConst.POLL_CYCLES_KEY,ConfigConst.DEFAULT_POLL_CYCLES);
+
+		if (this.pollRate >= 0) {
+			this.pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
+		}
 	}
 	
 	
@@ -49,12 +60,18 @@ public class SystemPerformanceManager
 	{
 	}
 	
-	public void startManager()
+	public boolean startManager()
 	{
+		_Logger.info("SystemPerformanceManager is starting...");
+
+		return true;
 	}
 	
-	public void stopManager()
+	public boolean stopManager()
 	{
+		_Logger.info("SystemPerformanceManager is stopped.");
+
+		return true;
 	}
 	
 }
