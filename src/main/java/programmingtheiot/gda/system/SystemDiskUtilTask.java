d43c13057a2444780f86ae7b7d2585daedce4305
Nicolas Gallo <nicolas.gallo800@gmail.com>
2025-02-28 14:29:47 +0100

    PIOT-GDA-05-002 implemented.

---
+++ src/main/java/programmingtheiot/gda/system/SystemDiskUtilTask.java
@@ -0,0 +1,31 @@
+/*
+package programmingtheiot.gda.system;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
+import java.util.logging.Logger;
+
+import programmingtheiot.common.ConfigConst;
+
+public class SystemDiskUtilTask extends BaseSystemUtilTask {
+
+    // private
+    private static final Logger _Logger = Logger.getLogger(BaseSystemUtilTask.class.getName());
+
+    // constructors
+
+    public SystemDiskUtilTask()
+    {
+        super(ConfigConst.NOT_SET, ConfigConst.DEFAULT_TYPE_ID);
+    }
+
+
+    // public methods
+
+    //@Override
+    //public float getTelemetryValue() {
+    //}
+    //TODO: Terminar de implementar el metodo abstracto getTelemetryValue() para extraer los valores de uso del disco.
+}
+
+*/
